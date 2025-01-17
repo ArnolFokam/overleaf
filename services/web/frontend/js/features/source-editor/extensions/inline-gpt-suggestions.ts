1c3d20e901bb212c625faa132a4f03c1d5758def
Arnol Fokam <arnolfokam23@gmail.com>
2025-01-18 01:23:59 +0200

    add call to GPT api

--- services/web/frontend/js/features/source-editor/extensions/inline-gpt-suggestions.ts
+++ services/web/frontend/js/features/source-editor/extensions/inline-gpt-suggestions.ts
@@ -22,7 +22,7 @@ import {
 import { OpenAI } from 'openai'
 
 const openai = new OpenAI({
-  //   apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
+  apiKey: '<INSERT_OPENAI_API_KEY>',
   dangerouslyAllowBrowser: true,
 })
 
